b5f40d2c0d97d2e73e46102dcad4339edc61756a
sawliq <55914680+sawliq@users.noreply.github.com>
2020-09-07 22:19:10 +0530

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,3 +1,3 @@
 import pyautogui
 img=pyautogui.screenshot()
-img.save(r"C:\Users\syeds\Desktop\d\dscreenshots.png")  // you can save it at any location
+img.save(r"C:\Users\syeds\Desktop\d\dscreenshots.png")  #you can save it at any location
