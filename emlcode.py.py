7163212e78619bfa0ec01b13ccb1d2055273f5f7
sawliq <55914680+sawliq@users.noreply.github.com>
2020-09-13 11:00:40 +0530

    code  to send email
    make sure you do not save your file name as email.py

---
+++ emlcode.py.py
@@ -0,0 +1,22 @@
+
+# Python code to illustrate Sending mail from  
+# your Gmail account  
+import smtplib 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login("snapcart55@gmail.com", "qwerty9876") 
+  
+# message to be sent 
+message = "hi i am your boss"
+  
+# sending the mail 
+s.sendmail("snapcart55@gmail.com", "sawlstore2@gmail.com", message) 
+  
+# terminating the session 
+s.quit() 
