907c6ccbe69cb6c70800d50ad45efd0184f92af5
sawliq <55914680+sawliq@users.noreply.github.com>
2020-09-13 11:02:16 +0530

    Update emlcode.py.py

--- emlcode.py.py
+++ emlcode.py.py
@@ -4,19 +4,19 @@
 import smtplib 
   
 # creates SMTP session 
-s = smtplib.SMTP('smtp.gmail.com', 587) 
+s = smtplib.SMTP('smtp.gmail.com', 587)   
   
 # start TLS for security 
 s.starttls() 
   
 # Authentication 
-s.login("snapcart55@gmail.com", "qwerty9876") 
+s.login("snapcart55@gmail.com", "qwerty9876")  #sender's email and password
   
 # message to be sent 
 message = "hi i am your boss"
   
 # sending the mail 
-s.sendmail("snapcart55@gmail.com", "sawlstore2@gmail.com", message) 
+s.sendmail("snapcart55@gmail.com", "sawlstore2@gmail.com", message)    #sender_email , receiver's email
   
 # terminating the session 
 s.quit() 
